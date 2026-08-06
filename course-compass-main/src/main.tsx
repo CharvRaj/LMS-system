1e45a3fa677a72e571801b6e93f5110a11285dee
RAJORACHARV <CHARVRAJ2006@gmail.com>
2026-08-06 18:51:07 +0530

    chore: enable admin https dev server and wrap app in ThemeProvider

--- course-compass-main/src/main.tsx
+++ course-compass-main/src/main.tsx
@@ -1,7 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { AppRouter } from "./routes/AppRouter";
+import { ThemeProvider } from "./context/ThemeContext";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <AppRouter />
+  <ThemeProvider>
+    <AppRouter />
+  </ThemeProvider>
 );
